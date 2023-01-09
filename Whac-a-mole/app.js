454ce8d2dcfc199384f2c8efce54034da02ba530
sanaeas <sanaassim1@gmail.com>
2023-01-09 08:46:12 +0100

    Whac-a-mole game

---
+++ Whac-a-mole/app.js
@@ -0,0 +1,49 @@
+const squares = document.querySelectorAll('.square');
+const mole = document.querySelector('.mole');
+const timeLeft = document.getElementById('time-left');
+const score = document.getElementById('score');
+
+let result = 0;
+let hitPosition;
+let currentTime = 60;
+let timerId = null;
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole');
+
+    hitPosition = randomSquare.id;
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++;
+            score.textContent = result;
+            hitPosition = null;
+        }
+    })
+})
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 600)
+}
+
+moveMole()
+
+function countDown() {
+    currentTime--;
+    timeLeft.textContent = currentTime;
+
+    if (currentTime == 0) {
+        clearInterval(countDownTimerId);
+        clearInterval(timerId);
+        alert("Game Over! Your final score is: " + result);
+    }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
